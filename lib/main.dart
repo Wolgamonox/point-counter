e1d0ade53e726df82411b5af294baa6366dc8f05
Gaston Wolfart <gastonwolfart@gmail.com>
2022-06-22 18:03:26 +0200

    added font

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,7 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 void main() {
+  GoogleFonts.config.allowRuntimeFetching = false;
   runApp(const MyApp());
 }
 
