6e6d00ecf7c6eff8cd4df3eab7c36c7b27f61169
Gaston Wolfart <gastonwolfart@gmail.com>
2024-06-24 00:25:36 +0200

    added riverpod dependency

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,7 @@
 import 'dart:collection';
 import 'dart:ui';
 
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/painting.dart';
 
 import 'package:google_fonts/google_fonts.dart';
 
