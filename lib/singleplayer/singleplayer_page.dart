6076216cf4efd89a83d63d7ca0bf61bde2e0cab8
Gaston Wolfart <gastonwolfart@gmail.com>
2024-06-25 00:10:45 +0200

    Started multiplayer

--- lib/singleplayer/singleplayer_page.dart
+++ lib/singleplayer/singleplayer_page.dart
@@ -4,7 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:point_counter/common/drawer.dart';
 
 Color progressToColor(double progress) {
-  double hue = progress * (120.0 - 10.0) + 10.0;
+  double hue = progress.clamp(0.0, 1.0) * (120.0 - 10.0) + 10.0;
   return HSLColor.fromAHSL(1.0, hue, 1.0, 0.4).toColor();
 }
 
